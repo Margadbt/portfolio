e32a670d14637f884ba83a229e706472a02cf023
unknown <margadbtt@gmail.com>
2024-07-10 17:18:08 +0800

    a

--- app/components/SinglePosterPreview/index.js
+++ app/components/SinglePosterPreview/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function SinglePosterPreview({imageSrc}){
     return(
         <div >          
-            <Image className="rounded-2xl hover:scale-[180%] transition-all duration-500 cursor-pointer hover:shadow-2xl" width={400} height={400} src={imageSrc} />
+            <Image className="rounded-2xl hover:scale-[180%] transition-all duration-500 cursor-pointer hover:shadow-2xl max-md:hover:scale-100" width={400} height={400} src={imageSrc} />
         </div>
     );
 }
